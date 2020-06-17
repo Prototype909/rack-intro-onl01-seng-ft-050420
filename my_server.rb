102c3c4d7bfd68e859cf03b3450900cf52aec4cd
Steve Anderson <prototype909@msn.com>
2020-06-17 04:56:54 +0000

    Automatically backed up by Learn

--- my_server.rb
+++ my_server.rb
@@ -1,4 +1,5 @@
 class MyServer
+  
   def call(env)
     return [ 200, {'Content-Type' => 'text/html'}, pretty_response ]
   end
@@ -6,4 +7,5 @@ class MyServer
   def pretty_response
     (Time.now.to_i % 2).zero? ?  ["<em>Hello</em>"] : ["<strong>Hello</strong>"]
   end
+  
 end
